7220ec25b414a1acb5db216c7b3835ab99c410bf
Alessio Ilardi <alessio.ilardi@gmail.com>
2023-01-18 14:42:04 +0100

    📦 NEW: "changeUsersAddress" function complete

--- src/3_immutability/immutability.js
+++ src/3_immutability/immutability.js
@@ -1,24 +1,24 @@
 const user10 = Object.freeze({
   id: 10,
-  name: 'Clementina DuBuque',
-  username: 'Moriah.Stanton',
-  email: 'Rey.Padberg@karina.biz',
+  name: "Clementina DuBuque",
+  username: "Moriah.Stanton",
+  email: "Rey.Padberg@karina.biz",
   address: Object.freeze({
-    street: 'Kattie Turnpike',
-    suite: 'Suite 198',
-    city: 'Lebsackbury',
-    zipcode: '31428-2261',
+    street: "Kattie Turnpike",
+    suite: "Suite 198",
+    city: "Lebsackbury",
+    zipcode: "31428-2261",
     geo: Object.freeze({
-      lat: '-38.2386',
-      lng: '57.2232',
+      lat: "-38.2386",
+      lng: "57.2232",
     }),
   }),
-  phone: '024-648-3804',
-  website: 'ambrose.net',
+  phone: "024-648-3804",
+  website: "ambrose.net",
   company: Object.freeze({
-    name: 'Hoeger LLC',
-    catchPhrase: 'Centralized empowering task-force',
-    bs: 'target end-to-end models',
+    name: "Hoeger LLC",
+    catchPhrase: "Centralized empowering task-force",
+    bs: "target end-to-end models",
   }),
 });
 
@@ -26,7 +26,20 @@ export const users = Object.freeze([user10]);
 
 // addressChanges è un oggetto che contiene una o più proprietà di Address da cambiare, ad esempio { city: London }
 // Ritornare l'array di utenti con le proprietà cambiate, mantenendo invariate quelle non presenti in addressChanges
-export const changeUsersAddress = (users, addressChanges) => {};
+export const changeUsersAddress = (users, addressChanges) => {
+  return users.map((user) => {
+    return {
+      ...user,
+      address: Object.entries(addressChanges).reduce(
+        (acc, [key, value]) => {
+          acc[key] = value;
+          return acc;
+        },
+        { ...user.address }
+      ),
+    };
+  });
+};
 
 // Ritornare l'array di utenti senza geo in address
 export const removeAddressCoordinates = (users) => {};
